b3d658dfb4fb7c275e8100333bb185078d105414
C1oudys <hymin310@gmail.com>
2024-01-24 12:23:37 +0900

    수정

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,17 +3,17 @@ import Header from "./Header";
 import InputForm from "./InputForm";
 import TodoList from "./TodoList";
 import Layout from "./Layout";
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
-const todoInitial = {
-    id: uuidv4(),
-    title: "",
-    content: "",
-    isDone: false,
-  };
+// const todoInitial = {
+//     id: uuidv4(),
+//     title: "Example Title",
+//     content: "Example Content",
+//     isDone: false,
+//   };
 
 const MainPage = () => {
-    const [todos, setTodos] = useState([todoInitial]);
+    const [todos, setTodos] = useState([]);
   
     const onSubmitTodo = (nextTodo) => {
       setTodos((prevTodos) => [nextTodo, ...prevTodos]);
